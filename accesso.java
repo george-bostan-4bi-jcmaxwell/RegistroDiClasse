027b67954743f543609ffc6403b46b9892a7a9fe
george-bostan-4bi-jcmaxwell <george.bostan@jcmaxwell.it>
2017-03-29 16:13:49 +0200

    Update accesso.java

--- accesso.java
+++ accesso.java
@@ -3,17 +3,24 @@ import java.io.*;
 import java.util.Scanner;
 
 public class accesso {
-    public accesso() throws InterruptedException
+    public accesso()
     {
       try 
       {
-        System.out.println ("  
+        System.out.println ("inserire il nome utente");
+        Scanner nome = new Scanner (System.in);
         FileReader reader = new FileReader("registrazione"+".txt");
         BufferedReader bufferedReader = new BufferedReader(reader); // cerca e legge il file 
         String line;
         while ((line = bufferedReader.readLine()) != null) // finchè non finisce di leggere il file, non finisce la lettura
         { 
-          if ()
+          if (nome == nomeUtente.registrazione())
+          {
+              System.out.println ("inserire la password");
+              Scanner pass = new Scanner (System.in);
+              if (pass == password.registrazione())
+              {
+                  
         }
           reader.close();
         } 
