135d820e84dc6f6cc9c25a27e0fb103db5d1e2f4
george-bostan-4bi-jcmaxwell <george.bostan@jcmaxwell.it>
2017-04-02 15:41:09 +0200

    Update Professori.java

--- Professori.java
+++ Professori.java
@@ -4,12 +4,19 @@ import java.util.Scanner;
 public class Professore {
   private String nome;
   private String cognome;
-  private String nomeUtente;
   private String materia;
-  private double votiMateria[];
-  private static final int votiMax = 10;
+  private double votiItaliano[];
+  private double votiMatematica[];
+  private double votiInglese[];
+  private double votiInformatica[];
+  private double votiTPSIT[];
+  private double votiTelecomunicazioni[];
+  private double votiSistemi[];
+  private double votiEducazioneFisica[];
+  private double votiStoria[];
+  private static final int votiMax = 50;
     
-  public Professore (String nome, String cognome, String nomeUtente, String materia) {
+  public Professore (String nome, String cognome, String materia) {
     setNome (nome);
     setCognome (cognome);
     setNomeUtente (nomeUtente);
@@ -20,59 +27,419 @@ public class Professore {
   private void setNome (String n) {nome = n;}
   public String getCognome () {return cognome;}
   private void setCognome (String c) {cognome = c;}
-  public String getNomeUtente () {return nomeUtente;}
-  private void setNomeUtente (String u) {nomeUtente = u;}
   
-  public void inserisciVoti() {
-    int i;
-    Scanner sc = new Scanner (System.in);
-    for (i=0; i<votiMax; i++)
+  public void menuProfessori() {
+    int numP;
+    
+    do
     {
-      votiMateria[i] = sc.nextDouble();
+        System.out.println ("Sceglire un numero: ");
+        System.out.println ("1 per inserire un voto");
+        System.out.println ("2 per togliere un voto");
+        System.out.println ("3 per visualizzare la media di uno studente");
+        System.out.println ("4 per uscire");
+        numP = sc.nextInt();
+        switch(numP)
+        {
+          case 1:
+            aggiungiVoti();
+            break;
+          case 2;
+            togliVoti();
+            break;
+          case 3:
+            visualizzaMedia();
+            break;
+          case 4;
+            System.out.println ("uscita...")
+            break;
+          default:
+            System.out.println ("il numero inserito e errato");
+            break;
+        }
     }
-  }
-  
-  public void togliVoti() {
-    int i, pos = 0, num;
-    bool trovato = false;
-    Scanner sc = new Scanner (System.in);
-    System.out.println("inserire il voto che si vuole togliere");
-    num = sc.nextDouble();
-    for (i=0; i<votiMax; i++)
-    {
-      if (num == votiMateria[i])
+    while (numP != 4)
+      
+    public void aggiungiVoti(){
+      int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0, i = 0;
+      double voto;
+      String inMateria;
+      System.out.println ("Sceglire la materia:");
+      System.out.println ("Italiano");
+      System.out.println ("Storia");
+      System.out.println ("Matematica");
+      System.out.println ("Informatica");
+      System.out.println ("Inglese");
+      System.out.println ("TPSIT");
+      System.out.println ("Telecomunicazioni");
+      System.out.println ("Sistemi");
+      System.out.println ("Educazione Fisica");
+      inMateria = sc.nextString;
+      switch(inMateria)
       {
-        pos = i;
-        trovato = true;
+        case "Italiano":
+          System.out.println ("inserire il voto");
+          voto = sc.nextDouble;
+          votiItaliano[a] = voto;
+          a++;
+          break;
+        case "Storia";
+          System.out.println ("inserire il voto");
+          voto = sc.nextDouble;
+          votiStoria[b] = voto;
+          b++;
+          break;
+        case "Matematica":
+          System.out.println ("inserire il voto");
+          voto = sc.nextDouble;
+          votiMatematica[c] = voto;
+          c++;
+          break;
+        case "Informatica";
+          System.out.println ("inserire il voto");
+          voto = sc.nextDouble;
+          votiInformatica[d] = voto;
+          d++;
+          break;
+        case "Inglese";
+          System.out.println ("inserire il voto");
+          voto = sc.nextDouble;
+          votiInglese[e] = voto;
+          e++;
+          break;
+        case "TPSIT";
+          System.out.println ("inserire il voto");
+          voto = sc.nextDouble;
+          votiTPSIT[f] = voto;
+          f++;
+          break;
+        case "Telecomunicazioni";
+          System.out.println ("inserire il voto");
+          voto = sc.nextDouble;
+          votiTelecomunicazioni[g] = voto;
+          g++;
+          break;
+        case "Sistemi";
+          System.out.println ("inserire il voto");
+          voto = sc.nextDouble;
+          votiSistemi[h] = voto;
+          h++;
+          break;
+        case "Educazione Fisica";
+          System.out.println ("inserire il voto");
+          voto = sc.nextDouble;
+          votiEducazioneFisica[i] = voto;
+          i++;
+          break;
+        default:
+          System.out.println ("La materia inserita non e stata scritta correttamente");
+          break;
       }
     }
-    if (trovato == true)
-    {
-      votiMateria[pos] = null;
+    
+    public void togliVoti(){
+      int pos, i;
+      boolean trovato = false;
+      String inMateria;
+      double elim;
+      System.out.println ("Sceglire la materia:");
+      System.out.println ("Italiano");
+      System.out.println ("Storia");
+      System.out.println ("Matematica");
+      System.out.println ("Informatica");
+      System.out.println ("Inglese");
+      System.out.println ("TPSIT");
+      System.out.println ("Telecomunicazioni");
+      System.out.println ("Sistemi");
+      System.out.println ("Educazione Fisica");
+      inMateria = sc.nextString;
+      switch(inMateria)
+      {
+        case "Italiano":
+          System.out.println ("inserire il voto che si vuole eliminare");
+          elim = sc.nextDouble;
+          for (i=0; i<votiMax; i++)
+          {
+            if (elim == votiItaliano[i])
+            {
+              trovato = true;
+              pos = i;
+            }
+          }
+          if (trovato == false)
+          {
+            System.out.println ("il voto non e stato trovato");
+          }
+          else
+          {
+            System.out.println ("il voto e stato eliminato");
+          }
+          break;
+        case "Storia";
+          System.out.println ("inserire il voto che si vuole eliminare");
+          elim = sc.nextDouble;
+          for (i=0; i<votiMax; i++)
+          {
+            if (elim == votiStoria[i])
+            {
+              trovato = true;
+              pos = i;
+            }
+          }
+          if (trovato == false)
+          {
+            System.out.println ("il voto non e stato trovato");
+          }
+          else
+          {
+            System.out.println ("il voto e stato eliminato");
+          }
+          break;
+        case "Matematica":
+          System.out.println ("inserire il voto che si vuole eliminare");
+          elim = sc.nextDouble;
+          for (i=0; i<votiMax; i++)
+          {
+            if (elim == votiMatematica[i])
+            {
+              trovato = true;
+              pos = i;
+            }
+          }
+          if (trovato == false)
+          {
+            System.out.println ("il voto non e stato trovato");
+          }
+          else
+          {
+            System.out.println ("il voto e stato eliminato");
+          }
+          break;
+        case "Informatica";
+          System.out.println ("inserire il voto che si vuole eliminare");
+          elim = sc.nextDouble;
+          for (i=0; i<votiMax; i++)
+          {
+            if (elim == votiInformatica[i])
+            {
+              trovato = true;
+              pos = i;
+            }
+          }
+          if (trovato == false)
+          {
+            System.out.println ("il voto non e stato trovato");
+          }
+          else
+          {
+            System.out.println ("il voto e stato eliminato");
+          }
+          break;
+        case "Inglese";
+          System.out.println ("inserire il voto che si vuole eliminare");
+          elim = sc.nextDouble;
+          for (i=0; i<votiMax; i++)
+          {
+            if (elim == votiInglese[i])
+            {
+              trovato = true;
+              pos = i;
+            }
+          }
+          if (trovato == false)
+          {
+            System.out.println ("il voto non e stato trovato");
+          }
+          else
+          {
+            System.out.println ("il voto e stato eliminato");
+          }
+          break;
+        case "TPSIT";
+          System.out.println ("inserire il voto che si vuole eliminare");
+          elim = sc.nextDouble;
+          for (i=0; i<votiMax; i++)
+          {
+            if (elim == votiTPSIT[i])
+            {
+              trovato = true;
+              pos = i;
+            }
+          }
+          if (trovato == false)
+          {
+            System.out.println ("il voto non e stato trovato");
+          }
+          else
+          {
+            System.out.println ("il voto e stato eliminato");
+          }
+          break;
+        case "Telecomunicazioni";
+          System.out.println ("inserire il voto che si vuole eliminare");
+          elim = sc.nextDouble;
+          for (i=0; i<votiMax; i++)
+          {
+            if (elim == votiTelecomunicazioni[i])
+            {
+              trovato = true;
+              pos = i;
+            }
+          }
+          if (trovato == false)
+          {
+            System.out.println ("il voto non e stato trovato");
+          }
+          else
+          {
+            System.out.println ("il voto e stato eliminato");
+          }
+          break;
+        case "Sistemi";
+          System.out.println ("inserire il voto che si vuole eliminare");
+          elim = sc.nextDouble;
+          for (i=0; i<votiMax; i++)
+          {
+            if (elim == votiSistemi[i])
+            {
+              trovato = true;
+              pos = i;
+            }
+          }
+          if (trovato == false)
+          {
+            System.out.println ("il voto non e stato trovato");
+          }
+          else
+          {
+            System.out.println ("il voto e stato eliminato");
+          }
+          break;
+        case "Educazione Fisica";
+         System.out.println ("inserire il voto che si vuole eliminare");
+          elim = sc.nextDouble;
+          for (i=0; i<votiMax; i++)
+          {
+            if (elim == votiEducazioneFisica[i])
+            {
+              trovato = true;
+              pos = i;
+            }
+          }
+          if (trovato == false)
+          {
+            System.out.println ("il voto non e stato trovato");
+          }
+          else
+          {
+            System.out.println ("il voto e stato eliminato");
+          }
+          break;
+        default:
+          System.out.println ("La materia inserita non e stata scritta correttamente");
+          break;
+      }
     }
-    else
-    {
-      System.out.println("non è stato trovato nessun voto uguale a quello inserito");
+    public String visualizzaMedia(){
+      int i;
+      double somma, media;
+      String inMateria;
+      System.out.println ("Sceglire la materia:");
+      System.out.println ("Italiano");
+      System.out.println ("Storia");
+      System.out.println ("Matematica");
+      System.out.println ("Informatica");
+      System.out.println ("Inglese");
+      System.out.println ("TPSIT");
+      System.out.println ("Telecomunicazioni");
+      System.out.println ("Sistemi");
+      System.out.println ("Educazione Fisica");
+      inMateria = sc.nextString;
+      switch(inMateria)
+      {
+        case "Italiano":
+          somma = 0;
+          for (i=0; i<votiMax; i++)
+          {
+            somma = somma + votiItaliano[i];
+          }
+          media = somma / votiMax;
+          System.out.println ("la media e: "+media)
+          break;
+        case "Storia";
+          somma = 0;
+          for (i=0; i<votiMax; i++)
+          {
+            somma = somma + votiStoria[i];
+          }
+          media = somma / votiMax;
+          System.out.println ("la media e: "+media)
+          break;
+        case "Matematica":
+          somma = 0;
+          for (i=0; i<votiMax; i++)
+          {
+            somma = somma + votiMatematica[i];
+          }
+          media = somma / votiMax;
+          System.out.println ("la media e: "+media)
+          break;
+        case "Informatica";
+          somma = 0;
+          for (i=0; i<votiMax; i++)
+          {
+            somma = somma + votiInformatica[i];
+          }
+          media = somma / votiMax;
+          System.out.println ("la media e: "+media)
+          break;
+        case "Inglese";
+          somma = 0;
+          for (i=0; i<votiMax; i++)
+          {
+            somma = somma + votiInglese[i];
+          }
+          media = somma / votiMax;
+          System.out.println ("la media e: "+media)
+          break;
+        case "TPSIT";
+          somma = 0;
+          for (i=0; i<votiMax; i++)
+          {
+            somma = somma + votiTPSIT[i];
+          }
+          media = somma / votiMax;
+          System.out.println ("la media e: "+media)
+          break;
+        case "Telecomunicazioni";
+          somma = 0;
+          for (i=0; i<votiMax; i++)
+          {
+            somma = somma + votiTelecomunicazioni[i];
+          }
+          media = somma / votiMax;
+          System.out.println ("la media e: "+media)
+          break;
+        case "Sistemi";
+          somma = 0;
+          for (i=0; i<votiMax; i++)
+          {
+            somma = somma + votiSistemi[i];
+          }
+          media = somma / votiMax;
+          System.out.println ("la media e: "+media)
+          break;
+        case "Educazione Fisica";
+          somma = 0;
+          for (i=0; i<votiMax; i++)
+          {
+            somma = somma + votiEducazioneFisica[i];
+          }
+          media = somma / votiMax;
+          System.out.println ("la media e: "+media)
+          break;
+        default:
+          System.out.println ("La materia inserita non e stata scritta correttamente");
+          break;
+      }
     }
-  }
-  
-  public double mediaVoti() {
-    double media;
-    int somma = 0;
-    int i;
-    for (i=0; i<votiMax; i++)
-    {
-      somma = somma + votiMateria[i];
-    }
-    media = somma / votiMax;
-    if (media >= 6)
-    {
-      System.out.println("PROMOSSO");
-    }
-    else
-    {
-      System.out.println("A RISCHIO BOCCIATURA");
-    }
-    return media;
-  }
-}
